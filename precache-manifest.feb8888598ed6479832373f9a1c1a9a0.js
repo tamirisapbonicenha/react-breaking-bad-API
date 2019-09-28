7c2a4cfbac1727827e91a137d5275effbcf5d177
Tamiris Bonicenha <tamirisapbonicenha@gmail.com>
2019-09-28 19:32:34 -0300

    Updates

---
+++ precache-manifest.feb8888598ed6479832373f9a1c1a9a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451ece439e0a9867f6c05fcb5aa78419",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4df21aaf5ababf40f6cc",
+    "url": "./static/js/2.a307da57.chunk.js"
+  },
+  {
+    "revision": "23f1cb15c2698400d74a",
+    "url": "./static/js/main.cbecfcf1.chunk.js"
+  },
+  {
+    "revision": "b05eb66e75f5ed1d3964",
+    "url": "./static/js/runtime-main.32930fc9.js"
+  },
+  {
+    "revision": "34e288b216714b5e9970981730c746f0",
+    "url": "./static/media/logo.34e288b2.jpg"
+  }
+]);
